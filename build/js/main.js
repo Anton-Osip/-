7aa7598d2956d6a73564a6021f068f4852b0f2cf
Anton <osipchika@gmail.com>
2020-09-29 22:13:53 +0300

    сверстал блок comments с адаптивом

--- build/js/main.js
+++ build/js/main.js
@@ -19,3 +19,25 @@ var priceSlider = new Swiper('.price__swiper', {
      }
 
 })
+
+var mySwiper = new Swiper('.comments__container', {
+     // Optional parameters
+     direction: 'horizontal',
+     loop: true,
+     spaceBetween: 10,
+
+
+     // If we need pagination
+     pagination: {
+          clickable: true,
+          type: 'bullets',
+          el: '.comments__pagination',
+     },
+
+     // Navigation arrows
+     navigation: {
+          nextEl: '.comments__button-next',
+          prevEl: '.comments__button-prev',
+     },
+
+})
